51d063e31317bd6bfc3710148312a4ebbeea91db
Oleksandr Fomin <qilibriumultra@gmail.com>
2022-05-20 19:24:30 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,7 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GOIT World");
+        System.out.println("This is our second commit");
+
     }
 }
